42656151ab0bd32ab9bc2a5adf49796aad83e857
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-23 14:09:34 -0500

    Creates the post model and index model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        desription: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [0, 140],
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
